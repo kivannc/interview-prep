a7e84e542cba389000149a84205a330bfee9ae41
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-17 22:52:08 +0300

    *

---
+++ LeetCode/HackerRank/ReversedDoublyLinkedList.cs
@@ -0,0 +1,31 @@
+﻿namespace InterviewPrep.HackerRank;
+
+internal partial class Questions
+{
+
+    public class DoublyLinkedListNode
+    {
+        public int Data { get; set; }
+        public DoublyLinkedListNode? Next { get; set; }
+        public DoublyLinkedListNode? Prev { get; set; }
+
+    }
+    public static DoublyLinkedListNode? Reverse(DoublyLinkedListNode? linkedList)
+    {
+
+        DoublyLinkedListNode? previousNode = null;
+        DoublyLinkedListNode? currentNode = linkedList;
+
+        while (currentNode != null)
+        {
+            previousNode = currentNode.Prev;
+            currentNode.Prev = currentNode.Next;
+            currentNode.Next = previousNode;
+            currentNode = currentNode.Prev;
+        }
+
+        linkedList = previousNode;
+        return linkedList;
+    }
+
+}
