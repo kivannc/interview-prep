e493c0b84bfc43d5941e51db5a3e8d1d96f31654
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-17 20:25:00 +0300

    *

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,13 +3,5 @@
 
 using InterviewPrep.HackerRank;
 
-Questions.ClimbingLeaderboard(new List<int> { 100, 100, 50, 40, 40, 20, 10 }, new List<int> { 5, 25, 50, 120 })
-    .ForEach(i => Console.Write("{0} ", i));
-Console.WriteLine("\n");
-Questions.ClimbingLeaderboard(new List<int> { 100, 90, 90, 80, 75, 60 }, new List<int> { 50, 65, 77, 90, 90, 90 })
-    .ForEach(i => Console.Write("{0} ", i));
-Console.WriteLine("\n");
-Questions.ClimbingLeaderboard(new List<int> { 100, 100, 90, 90, 80, 80, 70, 60, 60, 50 }, new List<int> { 45, 50, 75, 80, 102, 105, 120 })
-    .ForEach(i => Console.Write("{0} ", i));
-Console.WriteLine("\n");
+
 Console.ReadKey();
