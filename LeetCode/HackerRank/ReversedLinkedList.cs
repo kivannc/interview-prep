b1496f4478ac766cb103f2daa07e4303c38ed386
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-17 21:15:30 +0300

    ReversedLinkedList

---
+++ LeetCode/HackerRank/ReversedLinkedList.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPrep.HackerRank;
+
+internal partial class Questions
+{
+
+    public class SinglyLinkedListNode
+    {
+        public int Data { get; set; }
+        public SinglyLinkedListNode? Next { get; set; }
+
+    }
+    public static SinglyLinkedListNode? Reverse(SinglyLinkedListNode? linkedList)
+    {
+
+        SinglyLinkedListNode? previousNode = null;
+        SinglyLinkedListNode? currentNode = linkedList;
+
+        while (currentNode != null)
+        {
+            var nextNode = currentNode.Next;
+
+            currentNode.Next = previousNode;
+            currentNode = nextNode;
+        }
+
+        linkedList = previousNode;
+        return linkedList;
+    }
+
+}
