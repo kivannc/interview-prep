ae05a08c7c1bfedbc2c61ff9e4a1970ff2ba410f
Kivanc Erturk <erturkkivanc@gmail.com>
2023-02-10 18:25:35 +0300

    BinarySearchFinished

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -2,6 +2,5 @@
 using InterviewPrep.HackerRank;
 using InterviewPrep.LeetCode;
 
-Console.WriteLine(RemoveDublicatesFromSortedArray.RemoveDuplicates(new int[] { 1, 1, 2 }));
-
-Console.ReadKey();
+Console.WriteLine(BinarySearch.Search(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9));
+Console.WriteLine(BinarySearch.Search(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 11));
